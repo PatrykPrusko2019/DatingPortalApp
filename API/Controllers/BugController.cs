92209373e7dffe5d9d41269b3c24923ec0246bae
Patryk Prusko <patrykprusko@gmail.com>
2024-06-06 12:36:13 +0200

    I created a error controller for testing errors -> 'BugController'

---
+++ API/Controllers/BugController.cs
@@ -0,0 +1,51 @@
+
+using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BugController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BugController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret(){
+            
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound(){
+            
+            var thing = _context.Users.Find(-1);
+            if (thing == null) return NotFound();
+
+            return thing;
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError(){
+            
+            var thing = _context.Users.Find(-1);
+
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+        }
+
+        [HttpGet("bad-reguest")]
+        public ActionResult<string> GetBadReguest(){
+            
+
+            return BadRequest("this was not a good reguest");
+        }
+        
+    }
+}
